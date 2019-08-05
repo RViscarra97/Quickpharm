23063ab13f25fa98fac769cb0a441d616381f41e
Pablo Avelar <jp.avelar.m96@gmail.com>
2019-08-05 12:25:02 -0600

    Configuración de BD con MySQL

--- Quickpharm/settings.py
+++ Quickpharm/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'Quickpharm.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'quickpharm',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '3306',
     }
 }
 
